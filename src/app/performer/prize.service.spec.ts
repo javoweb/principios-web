69aac9dad19a6cb9469445d5b0cb6969bba2ae50
Gonzalo Tixilima <g.tixilima@uniandes.edu.co>
2021-02-23 14:38:05 -0500

    servicio prize implementado

--- src/app/performer/prize.service.spec.ts
+++ src/app/performer/prize.service.spec.ts
@@ -1,16 +1,52 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import {
+ HttpTestingController,
+ HttpClientTestingModule,
+} from '@angular/common/http/testing';
+
+import faker from 'faker';
+
 import { PrizeService } from './prize.service';
+import { Prize } from './prize';
+import { environment } from '../../environments/environment';
 
-describe('Service: Prize', () => {
+describe('PrizeService', () => {
+  let service: PrizeService;
+  let httpMock: HttpTestingController;
+  const apiUrl = environment.baseUrl + 'prizes/';
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PrizeService]
+      imports: [HttpClientTestingModule],
+      providers: [PrizeService],
     });
+
+    service = TestBed.inject(PrizeService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should ...', inject([PrizeService], (service: PrizeService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('getPrizes() should return correct records', () => {
+
+    const mockPosts = new Prize(
+    faker.name.findName(),
+      faker.name.findName(),
+      faker.lorem.sentence(),
+      []
+    );
+
+    const id: number = faker.random.number()
+
+    service.getPrize(id).subscribe((prize) => {
+      expect(prize.name).toBe(mockPosts.name);
+    });
+
+    const req = httpMock.expectOne(apiUrl + id.toString());
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPosts);
+  });
 });
