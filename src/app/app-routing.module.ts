3235f30f1c8240cfe7bd4e20fb19c2c79f42a03a
Gonzalo Tixilima <g.tixilima@uniandes.edu.co>
2021-03-01 17:59:53 -0500

    Crear routing Artista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AlbumListarComponent} from './album/album-listar/album-listar.component';
-import {CollectorListarComponent} from './collector/collector-listar/collector-listar.component';
+import { AlbumListarComponent } from './album/album-listar/album-listar.component';
+import { CollectorListarComponent } from './collector/collector-listar/collector-listar.component';
+import { PerformerComponent } from './performer/performer.component';
 
 const routes: Routes = [
   { path: 'albums', component: AlbumListarComponent },
+  { path: 'performers', component: PerformerComponent },
   { path: 'collectors', component: CollectorListarComponent },
   { path: '',   redirectTo: 'albums', pathMatch: 'full' }
 ];
